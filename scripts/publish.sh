77f35272c54860ade80923cf3999c4a2d71fec4a
pengpengcheng <1018423717@qq.com>
2017-07-11 11:59:09 +0800

    publish new version: v1.0.1

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -9,21 +9,38 @@ tag="v${version}";
 
 rm -f ./version.tmp;
 
-echo "1.[Dist] generate dist...";
+echo "[Dist] generate dist...";
 rm -rf dist;
 npm run dist;
 
-echo "2.[Package] Git commit package...";
+echo "[Package] Git commit package...";
 git pull;
 git add -A;
 git commit -m "publish new version: ${tag}";
 git push;
 
-echo "3.[NPM] NPM publish new version: ${tag}";
+echo "[NPM] NPM publish new version: ${tag}";
 git tag ${tag};
 git push origin ${tag};
 
-echo "4.[Package] Generate github page new version: ${tag}";
+echo "[Package] Generate github page new version: ${tag}";
 npm run vendor;
 npm run pages;
-cp builds tmp/ -r
+cp builds tmp/ -r && git checkout master;
+
+
+rm -rf builds/;
+cp tmp/builds/ builds -r;
+
+
+git pull;
+git add -A;
+git commit -m "update pages(master) new version: ${tag}";
+git push;
+
+echo "[Package] Back to src branch: subject new version: ${tag}";
+git checkout subject && rm -rf tmp;
+npm run vendor;
+npm restart;
+
+echo "[Publish] new version: ${tag}: Finished!"
