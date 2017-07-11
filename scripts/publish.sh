fd665f16b69525f7ec0b0d53248dcacaf52f512d
pengpengcheng <1018423717@qq.com>
2017-07-11 12:34:59 +0800

    publish new version: v1.0.1

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,65 +1,66 @@
 #!/usr/bin/env bash
+
 BLUE_COLOR='\E[1;34m'
 RES='\E[0m'
 echo "----------------------------------------------"
-echo -e "${BLUE_COLOR}[Publish] Start...${RES}"
+echo -e "[Publish] Start..."
 
 node ./scripts/generateReadMe.js;
 
 echo "------------------- 1 ------------------------"
-echo -e "${BLUE_COLOR}[Dist] get version...${RES}";
+echo -e "[Dist] get version...";
 version=`cat ./version.tmp`;
 tag="v${version}";
 
 rm -f ./version.tmp;
 
 echo "------------------- 2 ------------------------"
-echo -e "${BLUE_COLOR}[Dist] cp to dist...${RES}";
+echo -e "[Dist] cp to dist...";
 rm -rf dist;
 npm run dist;
 
 echo "------------------- 3 ------------------------"
-echo -e "${BLUE_COLOR}[Package] pull from SUBJECT...${RES}";
+echo -e "[Package] pull from SUBJECT...";
 git pull;
 git add -A;
 echo "------------------- 4 ------------------------"
-echo -e "${BLUE_COLOR}[Package] push into SUBJECT...${RES}";
+echo -e "[Package] push into SUBJECT...";
 git commit -m "publish new version: ${tag}";
 git push;
 
 echo "------------------- 5 ------------------------"
-echo -e "${BLUE_COLOR}[NPM] push tag to github...${RES}";
+echo -e "[NPM] push tag to github...";
 git tag ${tag};
 git push origin ${tag};
 
 echo "------------------- 6 ------------------------"
-echo -e "${BLUE_COLOR}[Package] run vendor && pages: ${tag}${RES}";
+echo -e "[Package] run vendor && pages: ${tag}";
 npm run vendor;
 npm run pages;
 echo "------------------- 7 ------------------------"
-echo -e "${BLUE_COLOR}[Package] copy to tmp from builds...${RES}";
+echo -e "[Package] copy to tmp from builds...";
 cp builds tmp/ -r
 
 echo "------------------- 8 ------------------------"
-echo -e "${BLUE_COLOR}[Github] checkout to master...${RES}";
+echo -e "[Github] checkout to master...";
 git checkout master;
 
 echo "------------------- 9 ------------------------"
-echo -e "${BLUE_COLOR}[Package] copy to builds from tmp...${RES}";
+echo -e "[Package] copy to builds from tmp...";
 rm -rf builds/;
 cp tmp/ builds -r;
 
 
 git pull;
 git add -A;
-git commit -m "update pages(master) new version: ${tag}${RES}";
+git commit -m "update pages(master) new version: ${tag}";
 git push;
 
 echo "------------------- 10 ------------------------"
-echo -e "${BLUE_COLOR}[Package] checkout to subject...${RES}";
+echo -e "[Package] checkout to subject...";
 git checkout subject && rm -rf tmp;
 npm run vendor;
 npm restart;
 
 echo "------------------- 11 ------------------------"
-echo -e "${BLUE_COLOR}[Publish] new version: ${tag}: Finished!${RES}"
+echo -e "[Publish] new version: ${tag}: Finished!"
