9639b24c5ad2e91a2f3dfcefe447b3feec51279e
pengpengcheng <1018423717@qq.com>
2017-07-11 21:14:56 +0800

    demo of MD to html

--- src/views/Note/index.js
+++ src/views/Note/index.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import $ from 'jquery';
+import { markdown } from 'markdown';
+
 import { Box, ButtonGroup, Button, A } from '../../../components';
 
 class Note extends React.Component {
@@ -13,6 +15,10 @@ class Note extends React.Component {
 	}
 
 	render() {
+		const $mdInter = markdown.toHTML('Hello *World*!');
+		const mdInterObj = {
+			__html: $mdInter,
+		}
 		const $toolbar = (
 			<ButtonGroup active={0}>
 				<Button type="default"><span className="fa fa-th" /> Heatmap</Button>
@@ -32,6 +38,8 @@ class Note extends React.Component {
 					<Box title="Button Group" toolBar={$toolbar}>
 						<p>Hello world</p>
 						<A href="https://github.com/evilstreak/markdown-js">https://github.com/evilstreak/markdown-js</A>
+						{$mdInter}
+						<div dangerouslySetInnerHTML={mdInterObj} />
 					</Box>
 				</section>
 			</div>
