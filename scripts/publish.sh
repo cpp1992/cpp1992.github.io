9f34f68fcfda493da2d99a81ae990ea1138a3eb2
pengpengcheng <1018423717@qq.com>
2017-07-11 10:24:41 +0800

    publish new version: v

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -2,42 +2,42 @@
 
 echo "[Publish] Start..."
 
-node ./scripts/generateReadMe.js
+node ./scripts/generateReadMe.js;
 
-version=`cat ./version.tmp`
-tag="v${version}"
+version=`cat ./version.tmp`;
+tag="v${version}";
 
-rm -f ./version.tmp
+rm -f ./version.tmp;
 
-echo "[Dist] generate dist..."
-rm -rf dist
-npm run dist
+echo "[Dist] generate dist...";
+rm -rf dist;
+npm run dist;
 
-echo "[Package] Git commit package..."
-git add -A
-git commit -m "publish new version: ${tag}"
-git push
+echo "[Package] Git commit package...";
+git add -A;
+git commit -m "publish new version: ${tag}";
+git push;
 
-echo "[NPM] NPM publish new version: ${tag}"
-git tag ${tag}
-git push origin ${tag}
+echo "[NPM] NPM publish new version: ${tag}";
+git tag ${tag};
+git push origin ${tag};
 
-echo "[Package] Generate github page new version: ${tag}"
-npm run vendor
-npm run pages
-rm -rf tmp/builds
-cp builds/ tmp/ -r
-git checkout master
-rm -rf builds/
-cp tmp/builds . -r
-git add -A
-git commit -m "update pages(master) new version: ${tag}"
-git push
-rm -rf tmp/
-echo "[Package] Back to src branch: subject new version: ${tag}"
-git checkout subject
-rm -rf tmp/
-npm run vendor
-npm restart
+echo "[Package] Generate github page new version: ${tag}";
+npm run vendor;
+npm run pages;
+rm -rf tmp/builds;
+cp builds/ tmp/ -r;
+git checkout master;
+rm -rf builds/;
+cp tmp/builds . -r;
+git add -A;
+git commit -m "update pages(master) new version: ${tag}";
+git push;
+rm -rf tmp/;
+echo "[Package] Back to src branch: subject new version: ${tag}";
+git checkout subject;
+rm -rf tmp/;
+npm run vendor;
+npm restart;
 
 echo "[Publish] new version: ${tag}: Finished!"
