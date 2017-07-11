a6bea95c75d0a7053f164edb431143caf55172a0
pengpengcheng <1018423717@qq.com>
2017-07-11 12:17:05 +0800

    publish new version: v1.0.1

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,25 +1,33 @@
 #!/usr/bin/env bash
-
-echo "[Publish] Start..."
+BLUE_COLOR='\E[1;34m'
+echo -e  "${BLUE_COLOR}----------------------------------------------${RES}"
+echo -e  "${BLUE_COLOR}[Publish] Start...${RES}"
 
 node ./scripts/generateReadMe.js;
 
+echo "------------------- 1 ------------------------"
+echo "[Dist] get version...";
 version=`cat ./version.tmp`;
 tag="v${version}";
 
 rm -f ./version.tmp;
 
-echo "[Dist] generate dist...";
+echo "------------------- 2 ------------------------"
+echo "[Dist] cp to dist...";
 rm -rf dist;
 npm run dist;
 
-echo "[Package] Git commit package...";
+echo "------------------- 3 ------------------------"
+echo "[Package] pull from SUBJECT...";
 git pull;
 git add -A;
+echo "------------------- 4 ------------------------"
+echo "[Package] push into SUBJECT...";
 git commit -m "publish new version: ${tag}";
 git push;
 
-echo "[NPM] NPM publish new version: ${tag}";
+echo "------------------- 5 ------------------------"
+echo "[NPM] publish to NPM registry...";
 git tag ${tag};
 git push origin ${tag};
 
@@ -30,7 +38,7 @@ cp builds tmp/ -r && git checkout master;
 
 
 rm -rf builds/;
-cp tmp/builds/ builds -r;
+cp tmp/ builds -r;
 
 
 git pull;
