3cb23ccf0006680b8ae33fdc25bd74c7fd8d12ef
pengpengcheng <1018423717@qq.com>
2017-07-11 11:10:12 +0800

    publish new version: v1.0.0

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -24,22 +24,12 @@ git tag ${tag};
 git push origin ${tag};
 
 echo "[Package] Generate github page new version: ${tag}";
+npm run vendor;
 npm run pages;
 rm -rf tmp/builds;
 cp builds/ tmp/ -r;
-git checkout master;
-rm -rf builds/;
-cp tmp/builds . -r;
 
-git pull;
-git add -A;
-git commit -m "update pages(master) new version: ${tag}";
-git push;
-rm -rf tmp/;
-echo "[Package] Back to src branch: subject new version: ${tag}";
-git checkout subject;
-rm -rf tmp/;
-npm run vendor;
-npm restart;
+git checkout master;
+
 
 echo "[Publish] new version: ${tag}: Finished!"
